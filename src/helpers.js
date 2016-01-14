2f5d39bd1bba933e07390f5f5909f2478df0a10e
Benjaco <benjaco1996@gmail.com>
2016-01-14 23:04:54 +0100

    helper class
    merge two option arrays

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Bejamco on 12-01-2016.
+ */
+
+var REST_helpers = {
+    option: function(option, default_options){
+        for(var settings in option) {
+            default_options[settings]=option[settings];
+        }
+        return default_options;
+    }
+};
